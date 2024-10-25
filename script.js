b549b77e1f6df6848dfc4cda8120653141dd6677
Meet Meghawala <105978113+meetmeghawala@users.noreply.github.com>
2024-10-25 21:38:17 +0530

    adding .gitmodules with submodule

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 60)
 });
+
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    console.log('Menu icon clicked'); // Check if this logs
+    navbar.classList.toggle('open');
+};
